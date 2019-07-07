50fa1e1d060c0b4421d9060daba147332a9b5bcf
Fei Liu <liu.feiwood@gmail.com>
2019-07-07 21:34:04 +0200

    add more configs

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,4 +1,16 @@
 export default {
   api: "https://dex.binance.org",
-  prefix: "bnb"
+  ws: "wss://dex.binance.org/api/ws",
+  evtNetwork: { host: "mainnet1.everitoken.io", port: 443, protocol: "https" },
+  db: {
+    host: "127.0.0.1",
+    user: "",
+    password: "",
+    database: "bnbbridge"
+  },
+  evtSwapAddresses: [],
+  binanceSwapAddress: "",
+  binanceChainSymbol: "EVT",
+  prefix: "bnb",
+  testnet: false
 };
