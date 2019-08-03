10dd915dbd12a6c94c8636a8afc695800b68191d
Fei Liu <liu.feiwood@gmail.com>
2019-08-03 14:29:50 +0200

    update config

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,11 +1,11 @@
 export default {
-  api: "https://dex.binance.org",
-  ws: "wss://dex.binance.org/api/ws",
-  evtNetwork: { host: "mainnet1.everitoken.io", port: 443, protocol: "https" },
-  db: "",
-  evtSwapAddresses: [],
-  binanceSwapAddress: "",
-  binanceChainSymbol: "EVT-xxx", // this need to changed
-  prefix: "bnb",
-  defaultProcessBlockNum: 1
-};
+  api: 'https://dex.binance.org',
+  ws: 'wss://dex.binance.org/api/ws',
+  evtNetwork: { host: 'mainnet7.everitoken.io', port: 443, protocol: 'https' },
+  db: 'postgres://xxx:xxx@localhost:5432/bnbbridge',
+  evtSwapAddresses: ['EVT5NYzcUjJEpGunhJJZmw8r2qph2uDhGBkhCWSJTp3Nv3VQ7aLT3'],
+  binanceSwapAddress: 'bnb1v3fl4kuwuhzf3g7ghscsq7uzmu5dw50waseptd',
+  binanceChainSymbol: 'EVT-49B',
+  prefix: 'bnb',
+  defaultProcessBlockNum: 1,
+}
